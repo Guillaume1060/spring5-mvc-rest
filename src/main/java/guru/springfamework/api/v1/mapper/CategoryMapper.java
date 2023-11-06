2d6f14837fbfcc582d9037e7e70004ac3dc3f360
Guillaume <breyerguillaume@gmail.com>
2023-11-06 10:59:59 +0100

    Setup MapStruct

---
+++ src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+// Use of MapStruct to map our Category DTO with our model
+// CategoryMapperImpl is created when compilation
+@Mapper
+public interface CategoryMapper {
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO (Category category);
+
+
+}
--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,5 +1,9 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Data;
+
+// Annotation @Data to setUp our CategoryDTO in compilation
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
