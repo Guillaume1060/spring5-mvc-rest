8ea8035bc1db3c7d8bb04010d237d3ced3d786d5
Guillaume <breyerguillaume@gmail.com>
2023-11-06 13:43:35 +0100

    Setup CategoryController

---
+++ src/test/java/guru/springfamework/controllers/CategoryControllerTest.java
@@ -0,0 +1,81 @@
+package guru.springfamework.controllers;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.controllers.v1.CategoryController;
+import guru.springfamework.services.CategoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class CategoryControllerTest {
+
+    public static final String NAME = "Bananes";
+    @Mock
+    CategoryService categoryService;
+    @InjectMocks // It automatically inject Mocks into the controller (here service)
+    CategoryController categoryController;
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        //  initialiser les objets simulés (mocks) dans une classe de test.
+        MockitoAnnotations.initMocks(this);
+
+        // Not necessary thanks to the InjectMocks
+        //categoryController = new CategoryController(categoryService);
+
+        // créer mockMvc pour simuler des requêtes HTTP vers le contrôleur
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    public void testListCategories() throws Exception {
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+        category1.setName(NAME);
+
+        CategoryDTO category2 = new CategoryDTO();
+        category2.setId(2L);
+        category2.setName("Poires");
+
+        List<CategoryDTO> categories = Arrays.asList(category1,category2);
+
+        when(categoryService.getAllCategories()).thenReturn(categories);
+
+        mockMvc.perform(get("/api/v1/categories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        // .categories" : C'est un chemin JSONPath qui permet d'accéder au champ "categories" dans le JSON renvoyé par la réponse.
+                .andExpect(jsonPath("$.categories",hasSize(2)));
+    }
+
+    @Test
+    public void testGetByNameCategories() throws Exception {
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1l);
+        category1.setName(NAME);
+
+        when(categoryService.getCategoryByName(anyString())).thenReturn(category1);
+
+        mockMvc.perform(get("/api/v1/categories/Bananes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)));
+    }
+}
